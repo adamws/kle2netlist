74abbc608c862fdcf1642fd0cd4283fc1d6a65c5
adamws <adamws@users.noreply.github.com>
2021-02-15 00:22:20 +0100

    add support for iso enter

--- kle2netlist/skidl.py
+++ kle2netlist/skidl.py
@@ -9,6 +9,7 @@ SUPPORTED_LIBRARIES = {
             "MX": {
                 "name": "MX_Only",
                 "footprint-nameformat": "MXOnly-{:g}U-NoLED",
+                "iso-enter": "MXOnly-ISO",
             },
             "Alps": {
                 "name": "Alps_Only",
@@ -17,6 +18,7 @@ SUPPORTED_LIBRARIES = {
             "MX/Alps Hybrid": {
                 "name": "MX_Alps_Hybrid",
                 "footprint-nameformat": "MX-{:g}U-NoLED",
+                "iso-enter": "MX-ISO",
             },
         },
         "supported-widths": [
@@ -44,6 +46,7 @@ SUPPORTED_LIBRARIES = {
             "MX": {
                 "name": "Switch_Keyboard_Cherry_MX",
                 "footprint-nameformat": "SW_Cherry_MX_PCB_{:.2f}u",
+                "iso-enter": "SW_Cherry_MX_PCB_ISOEnter_Rotated90",
             },
             "Alps": {
                 "name": "Switch_Keyboard_Alps_Matias",
@@ -77,10 +80,82 @@ SUPPORTED_LIBRARIES = {
 }
 
 
-def handle_key_matrix(keys, switch_module, supported_widths):
-    module_name = switch_module["name"]
-    footprint_format = switch_module["footprint-nameformat"]
+def is_iso_enter(key):
+    key_width = float(key["width"])
+    key_height = float(key["height"])
+    key_width_2 = float(key["width2"])
+    key_height_2 = float(key["height2"])
+    return (
+        key_width == 1.25
+        and key_height == 2
+        and key_width_2 == 1.5
+        and key_height_2 == 1
+    )
 
+
+def add_stabilizer(reference, key_width):
+    stabilizer_footprint = (
+        "Mounting_Keyboard_Stabilizer:Stabilizer_Cherry_MX_{:d}u".format(
+            math.trunc(key_width)
+        )
+    )
+    stabilizer = skidl.Part(
+        "Mechanical", "MountingHole", footprint=stabilizer_footprint
+    )
+    stabilizer.ref = reference
+
+
+def add_iso_enter_switch(switch_module):
+    module_name = switch_module["name"]
+
+    try:
+        switch_footprint = switch_module["iso-enter"]
+    except KeyError:
+        footprint_format = switch_module["footprint-nameformat"]
+        switch_footprint = f"{footprint_format}".format(1)
+
+    switch_footprint = f"{module_name}:{switch_footprint}"
+
+    switch = skidl.Part("Switch", "SW_Push", footprint=switch_footprint)
+    diode = skidl.Part("Device", "D", footprint="Diode_SMD:D_SOD-323F")
+
+    if (
+        module_name == "Switch_Keyboard_Cherry_MX"
+        or module_name == "Switch_Keyboard_Hybrid"
+    ):
+        stabilizer_footprint = (
+            "Mounting_Keyboard_Stabilizer:Stabilizer_Cherry_MX_2u"
+        )
+        stabilizer = skidl.Part(
+            "Mechanical", "MountingHole", footprint=stabilizer_footprint
+        )
+        switch_reference_number = switch.ref[2:]
+        stabilizer.ref = f"ST{switch_reference_number}"
+
+    return switch, diode
+
+
+def add_regular_switch(switch_module, key_width):
+    footprint_format = switch_module["footprint-nameformat"]
+    module_name = switch_module["name"]
+
+    switch_footprint = f"{footprint_format}".format(key_width)
+    switch_footprint = f"{module_name}:{switch_footprint}"
+
+    switch = skidl.Part("Switch", "SW_Push", footprint=switch_footprint)
+    diode = skidl.Part("Device", "D", footprint="Diode_SMD:D_SOD-323F")
+
+    if (
+        module_name == "Switch_Keyboard_Cherry_MX"
+        or module_name == "Switch_Keyboard_Hybrid"
+    ) and key_width >= 2:
+        switch_reference_number = switch.ref[2:]
+        add_stabilizer(f"ST{switch_reference_number}", key_width)
+
+    return switch, diode
+
+
+def handle_switch_matrix(keys, switch_module, supported_widths):
     rows = {}
     columns = {}
 
@@ -95,28 +170,13 @@ def handle_key_matrix(keys, switch_module, supported_widths):
         if not column in columns:
             columns[column] = skidl.Net(f"COL{column}")
 
-        key_width = float(key["width"])
-        if key_width not in supported_widths:
-            key_width = 1
-
-        switch_footprint = f"{footprint_format}".format(key_width)
-        switch_footprint = f"{module_name}:{switch_footprint}"
-
-        switch = skidl.Part("Switch", "SW_Push", footprint=switch_footprint)
-        diode = skidl.Part("Device", "D", footprint="Diode_SMD:D_SOD-323F")
-
-        if (
-            module_name == "Switch_Keyboard_Cherry_MX"
-            or module_name == "Switch_Keyboard_Hybrid"
-        ) and key_width >= 2:
-            stabilizer_footprint = "Mounting_Keyboard_Stabilizer:Stabilizer_Cherry_MX_{:d}u".format(
-                math.trunc(key_width)
+        if is_iso_enter(key):
+            switch, diode = add_iso_enter_switch(switch_module)
+        else:
+            key_width = float(key["width"])
+            switch, diode = add_regular_switch(
+                switch_module, key_width if key_width in supported_widths else 1
             )
-            stabilizer = skidl.Part(
-                "Mechanical", "MountingHole", footprint=stabilizer_footprint
-            )
-            stabilizer_reference = switch.ref[2:]
-            stabilizer.ref = f"ST{stabilizer_reference}"
 
         rows[row] += diode[1]
         columns[column] += switch[1]
@@ -141,7 +201,7 @@ def kle2netlist(layout, output_path, **kwargs):
     except KeyError as err:
         raise RuntimeError("Unsupported argument") from err
 
-    handle_key_matrix(layout["keys"], switch_module, supported_widths)
+    handle_switch_matrix(layout["keys"], switch_module, supported_widths)
 
     skidl.generate_netlist(file_=output_path)
 
